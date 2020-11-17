dd8c6f230243ce29dd5bf4a79cc381eec0c268e1
nathan <romeronathan@hotmail.com>
2020-11-17 18:13:41 -0500

    Angular Commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200").AllowAnyOrigin());
 
             app.UseAuthorization();
 
